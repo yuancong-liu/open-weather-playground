9d4be5e877f539fa8c5846fee42db42151ef25ff
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-02-23 19:38:48 +0900

    ✨ lint errors addressed

--- src/components/top/index.tsx
+++ src/components/top/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import useSWR from 'swr';
 
@@ -14,32 +14,8 @@ type Coord = {
 
 export const Top = () => {
   const [coord, setCoord] = useState<Coord>({ lon: 10, lat: 10 });
-  // const [valid, setValid] = useState<boolean>(false);
-  // const [counter, setCounter] = useState<number>(0);
 
-  // const getGeolocation = () =>
-  //   navigator.geolocation.getCurrentPosition((position) => {
-  //     const { latitude, longitude } = position.coords;
-  //     console.log(latitude, longitude);
-
-  //     setCoord({
-  //       lon: longitude,
-  //       lat: latitude,
-  //     });
-  //   });
-
-  // useEffect(() => {
-  //   if ('geolocation' in navigator) {
-  //     console.log('geolocation is available');
-  //     setValid(true);
-  //     return;
-  //   }
-  //   setCounter(counter + 1);
-  // }, [counter]);
-
-  // if (valid) {
-  //   getGeolocation();
-  // }
+  setCoord({ lon: 10, lat: 10 });
 
   const fetcher = (url: string, lon: number, lat: number, apiKey: string) =>
     axiosInstance
@@ -50,8 +26,6 @@ export const Top = () => {
     [`/weather`, coord.lon, coord.lat],
     () => fetcher('/weather', coord.lon, coord.lat, API_KEY),
   );
-
-  console.log(data, error, isLoading);
 
   if (isLoading) return <div>Loading...</div>;
 
